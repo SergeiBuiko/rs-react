0cdcfba15c3ed5510100b1879c555515f168fc91
Sergei Buiko <sergeibuiko1@gmail.com>
2023-03-28 02:40:54 +0300

    feat: add name validation

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,11 +6,9 @@ import React from 'react';
 export function MainPage() {
   return (
     <div>
-      {/* <input type="text" className="search-field" /> */}
       <SearchBar />
       <h1>Current product list</h1>
       <Product />
     </div>
   );
 }
-// export { Homepage };
