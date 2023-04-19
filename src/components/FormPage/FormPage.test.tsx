3779682a197a5f20071f425581cae2d6ca754d3f
Sergei Buiko <sergeibuiko1@gmail.com>
2023-04-19 20:14:42 +0300

    fix: fix testing errors

--- src/components/FormPage/FormPage.test.tsx
+++ src/components/FormPage/FormPage.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-
+import { Provider } from 'react-redux';
 import FormPage from './FormPage';
+import { store } from 'index';
 
 describe('FormPage', () => {
   it('Renders FormPage', () => {
-    render(<FormPage />);
+    render(
+      <Provider store={store}>
+        <FormPage />
+      </Provider>
+    );
+    // (<FormPage />);
     expect(screen.getByText(/Product name/i)).toBeInTheDocument();
     expect(screen.getByText(/Production date/i)).toBeInTheDocument();
   });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { setupStore } from 'store/store';
 import App from './App';
-const store = setupStore();
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+import { render } from '@testing-library/react';
+export const store = setupStore();
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+render(
   <Provider store={store}>
     <App />
   </Provider>
